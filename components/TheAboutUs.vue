1203c1df50c85727c3fc6e9688feb23f3c6bf3da
yordan.ramchev <yordan.ramchev@mentormate.com>
2023-04-12 20:13:01 +0300

    Initial project setup

--- components/TheAboutUs.vue
+++ components/TheAboutUs.vue
@@ -6,39 +6,55 @@ const { t } = useI18n();
 
 <template>
   <VSection class="pt-0">
-    <div class="px-4 py-6 md:px-8 md:py-12 bg-gray-50 rounded flex">
-      <div class="pr-8 w-1/2">
-        <h2>{{ t("aboutUs") }}</h2>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima fugiat id
-          officiis eius ratione cum laudantium ipsum eveniet incidunt sequi vero ullam
-          illo, beatae enim dolore, ea eaque? Eos, officiis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima fugiat id
-          officiis eius ratione cum laudantium ipsum eveniet incidunt sequi vero ullam
-          illo, beatae enim dolore, ea eaque? Eos, officiis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima fugiat id
-          officiis eius ratione cum laudantium ipsum eveniet incidunt sequi vero ullam
-          illo, beatae enim dolore, ea eaque? Eos, officiis?
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima fugiat id
-          officiis eius ratione cum laudantium ipsum eveniet incidunt sequi vero ullam
-          illo, beatae enim dolore, ea eaque? Eos, officiis?
-        </p>
-        <nuxt-link to="/" class="button button--primary mt-6"
-          >{{ t("aboutUs") }}
-        </nuxt-link>
-      </div>
-      <div class="pl-8 w-1/2 rounded">
-        <img
-          :src="faker.image.city(600, 480, true)"
-          alt=""
-          class="rounded h-full object-cover"
-        />
+    <div class="mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
+      <div class="grid gap-10 lg:grid-cols-2">
+        <div class="lg:pr-10">
+          <a
+            href="/"
+            aria-label="Go Home"
+            title="Logo"
+            class="inline-block mb-5"
+          >
+            <div
+              class="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50"
+            >
+              <svg
+                class="w-10 h-10 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  stroke-width="3"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                ></polygon>
+              </svg>
+            </div>
+          </a>
+          <h5 class="mb-4 text-4xl font-extrabold leading-none">
+            The quick, brown fox<br class="hidden md:block" />
+            jumps over
+            <span class="inline-block text-deep-purple-accent-400"
+              >a lazy dog</span
+            >
+          </h5>
+          <p class="mb-6 text-gray-900">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae explicabo. Sed ut perspiciatis unde omnis iste natus error sit
+            voluptatem totam rem aperiam, eaque ipsa quae explicabo.
+          </p>
+          <hr class="mb-5 border-gray-300" />
+        </div>
+        <div>
+          <img
+            class="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+            src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   </VSection>
