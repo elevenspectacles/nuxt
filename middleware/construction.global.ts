9c55cbc11ac52d014206b6580727ed7f75bea139
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-02-14 16:00:19 +0200

    Initial project setup

--- middleware/construction.global.ts
+++ middleware/construction.global.ts
@@ -1,7 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
   if (
     process.env.NODE_ENV === "production" &&
-    to.path !== "/under-construction"
+    to.path !== "/under-construction" &&
+    to.path.indexOf("/st") > -1
   ) {
     return navigateTo("/under-construction");
   }
