6b0e74bbc77cc5ab1d0eb58796ca4677c9dac17f
yordan.ramchev <yordan.ramchev@mentormate.com>
2024-02-15 17:15:39 +0200

    Initial project setup

--- storyblok/Static.vue
+++ storyblok/Static.vue
@@ -7,5 +7,46 @@ const articleContent = computed(() => {
 </script>
 
 <template>
-  <div v-html="articleContent"></div>
+  <div v-html="articleContent" class="st"></div>
 </template>
+
+<style>
+.st {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 600;
+    line-height: 1.5;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  h2 {
+    font-size: 1.75rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  h4 {
+    font-size: 1.25rem;
+  }
+
+  h5 {
+    font-size: 1rem;
+  }
+
+  p {
+    font-size: 1rem;
+  }
+  p + p {
+    margin-top: 12px;
+  }
+}
+</style>
