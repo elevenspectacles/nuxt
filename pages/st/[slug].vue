2d110222fda7d134a8140cba907147bc80a31b7b
Yordan Ramchev <yordan.ramchev@Yordans-MacBook-Pro.local>
2024-04-17 21:11:00 +0300

    feat: update static page styles

--- pages/st/[slug].vue
+++ pages/st/[slug].vue
@@ -60,82 +60,62 @@ const parsedMD = computed(() =>
 
 <style>
 .static-page {
-  max-width: 820px;
-  margin: 0 auto;
+  @apply mx-auto max-w-screen-md;
 
-  h1,
   h2,
   h3,
   h4,
   h5,
   h6 {
-    font-weight: 600;
-    line-height: 1.5;
+    @apply font-bold leading-7;
+  }
+
+  h2 {
+    @apply text-4xl;
+  }
+
+  h3 {
+    @apply text-3xl;
+  }
+
+  h4 {
+    @apply text-2xl;
+  }
+
+  h5 {
+    @apply text-xl;
+  }
+
+  p {
+    @apply text-lg leading-7;
+  }
+
+  p + *,
+  pre + *,
+  li + li,
+  ol + *,
+  ul + * {
+    @apply mt-4;
   }
 
   h2,
   h3,
   h4,
   h5 {
-    @apply mt-6 mb-4;
-  }
-
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 24px;
-  }
-
-  h2 {
-    font-size: 1.75rem;
-  }
-
-  h3 {
-    font-size: 1.5rem;
-  }
-
-  h4 {
-    font-size: 1.25rem;
-  }
-
-  h5 {
-    font-size: 1rem;
-  }
-
-  p {
-    font-size: 1rem;
-  }
-
-  p + p {
-    margin-top: 12px;
+    @apply mt-12 mb-4;
   }
 
   a {
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    text-decoration: underline;
+    @apply text-lg leading-7 underline;
   }
 
   li {
     @apply relative pl-5;
   }
 
-  li + li,
-  ul + p,
-  p + ol,
-  ol + p {
-    @apply mt-4;
-  }
-
   ul li::before {
     content: "";
-    @apply absolute;
-    left: 5px;
-    top: 9px;
-    background-color: #084c61;
-    display: block;
-    width: 5px;
-    height: 5px;
-    border-radius: 100%;
+    @apply absolute left-1 top-2 bg-primary-600 block w-2 h-2 rounded-full;
   }
 
   ol li {
@@ -144,11 +124,11 @@ const parsedMD = computed(() =>
 
   ol li::before {
     content: counter(step-counter) ".";
-    @apply absolute block;
-    color: #084c61;
-    font-size: 1rem;
-    font-weight: bold;
-    left: 4px;
+    @apply absolute block text-primary-600 font-bold text-base left-0;
+  }
+
+  img {
+    @apply rounded-md mb-6;
   }
 }
 </style>
